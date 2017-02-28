bbfe66ab1e8c9ee72aa0a137f7fe904d31a5a19a
wusonglin <wusonglin@moqikaka.com>
2017-02-28 15:26:13 +0800

    no message

---
+++ src/app/scenes/MapLayer.lua
@@ -0,0 +1,20 @@
+--
+-- Author: Your Name
+-- Date: 2017-02-28 15:19:23
+--
+local MapLayer = class("MapLayer", function()
+    return display.newLayer("MapLayer")
+end)
+
+local NUM_ROW = 8    -- 行
+local NUM_COL = 8    -- 列
+
+function MapLayer:ctor()
+	
+	
+
+
+
+end
+
+return MapLayer
